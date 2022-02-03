ee25795ae09d31e473e23bfb061164e8b24b5178
Sean <you@example.com>
2022-02-04 01:26:37 +0900

    3rd day

---
+++ 예산.py
@@ -0,0 +1,14 @@
+"""https://programmers.co.kr/learn/courses/30/lessons/12982"""
+
+def solution(d, budget):
+    if sum(d) <= budget:
+        return len(d)
+    else:
+        d.sort()
+        cnt = 0
+        for i in d:
+            budget -= i
+            if budget < 0:
+                break
+            cnt +=1
+        return cnt
