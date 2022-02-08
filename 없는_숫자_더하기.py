3ad37d628981ca76c8f963b6a65cfca9957dbc22
Sean Park <seyeonpark@berkeley.edu>
2022-02-08 09:08:00 +0900

    2.8

---
+++ 없는_숫자_더하기.py
@@ -0,0 +1,9 @@
+"https://programmers.co.kr/learn/courses/30/lessons/86051"
+
+#1st try
+def solution(numbers):
+    answer = 0
+    for i in range(0, 10):
+        if i not in numbers:
+            answer += i 
+    return answer
